772ec6c3c910842746f67be27e4ddaf90b7f7efd
StounhandJ <rmarinicev@gmail.com>
2022-09-29 00:07:24 +0300

    hot fix

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,11 +10,11 @@ class SearchController extends Controller
 {
     public function book(SearchRequest $request): \Illuminate\Http\JsonResponse
     {
-        return  response()->json(["message" => "success", "products" => Book::search($request)->get()]);
+        return  response()->json(["message" => "success", "response" => Book::search($request->get("name"))->get()]);
     }
 
     public function author(SearchRequest $request): \Illuminate\Http\JsonResponse
     {
-        return  response()->json(["message" => "success", "products" => Author::search($request)->get()]);
+        return  response()->json(["message" => "success", "response" => Author::search($request->get("name"))->get()]);
     }
 }
